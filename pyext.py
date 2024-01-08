cb848164eefbd265917053ca94477d266862fab4
Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
2024-01-08 11:03:20 +0100

    Update pyext.py to fit Python 3.11

--- pyext.py
+++ pyext.py
@@ -30,7 +30,11 @@ __all__ = ['overload', 'RuntimeModule', 'switch', 'tail_recurse', 'copyfunc',
            'call_if_main', 'run_main']
 
 import sys, inspect, types, functools
+import inspect
 
+if not hasattr(inspect, 'getargspec'):
+    inspect.getargspec = inspect.getfullargspec
+           
 def _targspec(func, specs, attr='__orig_arg__'):
     if hasattr(func, '__is_overload__') and func.__is_overload__:
         return getattr(func, attr)
